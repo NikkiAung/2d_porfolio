53f93469bda569bb599a62d3b71a81e98d5d9b02
Aung Nanda Oo <aungnandaoo.425@gmail.com>
2025-01-05 14:58:23 -0800

    adding some components and creating necessary files for the entire project

---
+++ src/main.jsx
@@ -0,0 +1,15 @@
+import { createRoot } from "react-dom/client";
+import { StrictMode } from "react";
+import initGame from "./initGame";
+import ReactUI from "./ReactUI";
+
+const ui = document.getElementById("ui");
+const root = createRoot(ui);
+
+root.render(
+    <StrictMode>
+        <ReactUI/>
+    </StrictMode>
+);
+
+initGame()
